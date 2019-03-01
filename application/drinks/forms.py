fc0897e9b4871dfd07be01062909d8227154323d
Karoliina Koivu <karoliinakoivu@gmail.com>
2019-03-01 21:26:27 +0200

    SelectField toivottavasti toimimaan herokussa yritys nro.2

--- application/drinks/forms.py
+++ application/drinks/forms.py
@@ -11,7 +11,7 @@ def drinktype_query():
 
 class DrinkForm(FlaskForm):
     name = StringField("Nimi:", [validators.InputRequired(), validators.length(3, 20, message='Nimen on oltava vähintään 3 ja korkeintaan 20 merkkiä pitkä')])
-    drinktype = SelectField("Juomalaji:", choices=drinktype_query())
+    drinktype = SelectField("Juomalaji:", coerce=int, choices=drinktype_query())
     instructions = TextAreaField("Ohjeet: ", [validators.InputRequired(), validators.length(10, 280, message='Ohjeen on oltava vähintään 10 ja korkeintaan 280 merkkiä pitkä')])
     submit = SubmitField("Lisää drinkki")
 
